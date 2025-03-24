6576dd1ec330a14941eb50e29433faacc9f7aaef
Andrew <andy.matt89@gmail.com>
2025-03-24 17:36:21 +1100

    Updated FileGroupDescriptor
    Updated to leverage marshalling and remove redundant code

--- Drag&DropDebugger/DataHandlers/FileGroupDescriptor.cs
+++ Drag&DropDebugger/DataHandlers/FileGroupDescriptor.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -14,79 +14,43 @@ namespace Drag_DropDebugger.DataHandlers
     internal class FileGroupDescriptor
     {
         [Flags]
-        enum Group_dwFlag : uint
+        public enum FILE_DESCRIPTOR_FLAGS : uint
         {
-            FD_CLSID = 0x1,
-            FD_SIZEPOINT = 0x2,
-            FD_ATTRIBUTES = 0x4,
-            FD_CREATETIME = 0x8,
-            FD_ACCESSTIME = 0x10,
-            FD_WRITESTIME = 0x20,
-            FD_FILESIZE = 0x40,
-            FD_PROGRESSUI = 0x4000,
-            FD_LINKUI = 0x8000,
-            FD_UNICODE = 0x80000000,
+            CLSID = 0x1,
+            SIZEPOINT = 0x2,
+            ATTRIBUTES = 0x4,
+            CREATETIME = 0x8,
+            ACCESSTIME = 0x10,
+            WRITESTIME = 0x20,
+            FILESIZE = 0x40,
+            PROGRESSUI = 0x4000,
+            LINKUI = 0x8000,
+            UNICODE = 0x80000000,
         }
 
-        private static string FlagstoString(uint dwFlags)
+        public enum FILE_ATTRIBUTE_FLAGS : uint
         {
-            string flagStr = "( 0b" + Convert.ToString(dwFlags, 2).PadLeft(8,'0') + ")" ;
-            foreach (Group_dwFlag _dwFlag in Enum.GetValues(typeof(Group_dwFlag)))
-            {
-                flagStr += ((dwFlags & (uint)_dwFlag) != 0) ? " | " + Enum.GetName(typeof(Group_dwFlag), _dwFlag) : "";
-            }
-            return flagStr;
-        }
-
-        private static string FileAttributestoString(uint dwFileAttributes)
-        {
-            string attribStr = "( 0b" + Convert.ToString(dwFileAttributes, 2).PadLeft(8, '0') + ")";
-            foreach (FileAttributes _fileAttri in Enum.GetValues(typeof(FileAttributes)))
-            {
-                attribStr += ((dwFileAttributes & (uint)_fileAttri) != 0) ? " | " + Enum.GetName(typeof(FileAttributes), _fileAttri) : "";
-            }
-            return attribStr;
-        }
-
-        private static string GetTimeString(System.Runtime.InteropServices.ComTypes.FILETIME fileTime)
-        {
-            ulong high = (ulong)fileTime.dwHighDateTime;
-            uint low = (uint)fileTime.dwLowDateTime;
-            long fileTime64 = (long)((high << 32) + low);
-            if (fileTime64 == 0)
-                return "NA";
-
-            try
-            {
-                DateTime dateTime = DateTime.FromFileTimeUtc(fileTime64);
-                return dateTime.ToShortDateString() + " " + dateTime.ToLongTimeString();
-            }
-            catch
-            {
-                return "NA";
-            }
-        }
-
-        private static string GetFileSizeString(uint fileSizeHigh, uint fileSizeLow)
-        {
-            string[] bytePeriods = { " B", " KB", " MB", " GB", " TB" };
-
-            long fileSizeBytes = (fileSizeHigh << 32) + fileSizeLow;
-            string byteSizeStr = fileSizeBytes.ToString("N0") + " bytes";
-
-            if(fileSizeBytes < 1024)
-                return byteSizeStr;
-
-            int period = 0;
-            double fileSizeConv = fileSizeBytes;
-            while (fileSizeConv > 1024)
-            {
-                fileSizeConv /= 1024;
-                period++;
-            }
-            fileSizeConv = Math.Round(fileSizeConv, period > 2 ? 2 : 0);
-
-            return $"{fileSizeConv}{bytePeriods[period]} ({byteSizeStr})";
+            READONLY = 0x1,
+            HIDDEN = 0x2,
+            SYSTEM = 0x4,
+            DIRECTORY = 0x10,
+            ARCHIVE = 0x20,
+            DEVICE = 0x40,
+            NORMAL = 0x80,
+            TEMPORARY = 0x100,
+            SPARSE_FILE = 0x200,
+            REPARSE_POINT = 0x400,
+            COMPRESSED = 0x800,
+            OFFLINE = 0x1000,
+            NOT_CONTENT_INDEXED = 0x2000,
+            ENCRYPTED = 0x4000,
+            INTEGRITY_STREAM = 0x8000,
+            VIRTUAL = 0x10000,
+            NO_SCRUB_DATA = 0x20000,
+            EA_OR_RECALL_ON_OPEN = 0x40000,
+            PINNED = 0x80000,
+            UNPINNED = 0x100000,
+            RECALL_ON_DATA_ACCESS = 0x400000,
         }
 
         [StructLayout(LayoutKind.Sequential)]
@@ -94,6 +58,11 @@ namespace Drag_DropDebugger.DataHandlers
         {
             public int x;
             public int y;
+
+            public override string ToString()
+            {
+                return $"({x},{y})";
+            }
         }
 
         [StructLayout(LayoutKind.Sequential)]
@@ -101,6 +70,11 @@ namespace Drag_DropDebugger.DataHandlers
         {
             public int cx;
             public int cy;
+
+            public override string ToString()
+            {
+                return $"({cx},{cy})";
+            }
         }
 
 
@@ -114,11 +88,11 @@ namespace Drag_DropDebugger.DataHandlers
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
         public sealed class FILEDESCRIPTORW
         {
-            public uint dwFlags;
+            public FILE_DESCRIPTOR_FLAGS dwFlags;
             public Guid clsid;
             public SIZEL sizel;
             public POINTL pointl;
-            public uint dwFileAttributes;
+            public FILE_ATTRIBUTE_FLAGS dwFileAttributes;
             public System.Runtime.InteropServices.ComTypes.FILETIME ftCreationTime;
             public System.Runtime.InteropServices.ComTypes.FILETIME ftLastAccessTime;
             public System.Runtime.InteropServices.ComTypes.FILETIME ftLastWriteTime;
@@ -129,16 +103,11 @@ namespace Drag_DropDebugger.DataHandlers
         }
 
         //https://stackoverflow.com/questions/58120399/c-sharp-drag-and-drop-from-outlook-using-net-4-5-or-later
-        static void ProcessFileDescriptor(TabControl TabCtrl, MemoryStream fileGroupStream)
+        static void ProcessFileDescriptor(TabControl TabCtrl, byte[] fileGroupDescriptorBytes)
         {
             IntPtr fileGroupDescriptorWPointer = IntPtr.Zero;
             try
             {
-                //use the underlying IDataObject to get the FileGroupDescriptorW as a MemoryStream
-                fileGroupStream.Position = 0;
-                byte[] fileGroupDescriptorBytes = new byte[fileGroupStream.Length];
-                fileGroupStream.Read(fileGroupDescriptorBytes, 0, fileGroupDescriptorBytes.Length);
-
                 //copy the file group descriptor into unmanaged memory
                 fileGroupDescriptorWPointer = Marshal.AllocHGlobal(fileGroupDescriptorBytes.Length);
                 Marshal.Copy(fileGroupDescriptorBytes, 0, fileGroupDescriptorWPointer, fileGroupDescriptorBytes.Length);
@@ -178,22 +147,8 @@ namespace Drag_DropDebugger.DataHandlers
             if (desc == null)
                 return;
 
-            TabHelper.AddStringListTab(tabCtrl, desc.cFileName, new string[]
-            {
-                $"FileName: {desc.cFileName}",
-                $"FileSize: {GetFileSizeString(desc.nFileSizeHigh, desc.nFileSizeLow)}",
-                "",
-                $"dwFlags: {FlagstoString(desc.dwFileAttributes)}",
-                $"clsid: {desc.clsid}",
-                $"sizel: ({desc.sizel.cx},{desc.sizel.cy})",
-                $"pointl: ({desc.pointl.x},{desc.pointl.y})",
-                $"dwFileAttributes: {FileAttributestoString(desc.dwFileAttributes)}",
-                $"ftCreationTime: {GetTimeString(desc.ftCreationTime)}",
-                $"ftLastAccessTime: {GetTimeString(desc.ftLastAccessTime)}",
-                $"ftLastWriteTime: {GetTimeString(desc.ftLastWriteTime)}",
-                $"nFileSizeHigh: {desc.nFileSizeHigh.ToString()}",
-                $"nFileSizeLow: {desc.nFileSizeLow.ToString()}",
-                $"cFileName: {desc.cFileName}"});
+            string[] propertyStrings = StringHelper.ClassToString(desc);
+            TabHelper.AddStringListTab(tabCtrl, desc.cFileName, propertyStrings);
         }
 
         public TabControl mTabReference;
