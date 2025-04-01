207c29d744c9cf29943caa4ec1bac1c52f5a34ae
Andrew <andy.matt89@gmail.com>
2025-04-01 19:12:00 +1100

    Added a Image Preview Panel support to StackDataTab

--- Drag&DropDebugger/UI/StackedDataTab.cs
+++ Drag&DropDebugger/UI/StackedDataTab.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Drag_DropDebugger.UI
 {
@@ -28,13 +30,13 @@ namespace Drag_DropDebugger.UI
             AddChild(mScrollViewer);
         }
 
-        public void AddDataGrid(string label, Dictionary<String, Object> data, int indexPos = -1)
+        private void AddUIElement(string label, object element, int indexPos = -1)
         {
             GroupBox groupBox = new GroupBox()
             {
                 Header = label,
-                Content = new DictionaryGridView(data),
-                Margin = new Thickness(0, 0, 0, 10)
+                Content = element,
+                Margin = new Thickness(0, 0, 0, 10),
             };
 
             if (!(indexPos >= 0 && indexPos < mChildren.Count))
@@ -44,13 +46,48 @@ namespace Drag_DropDebugger.UI
 
             mChildren.Insert(indexPos, groupBox);
 
-
-
             mPanel.Children.Clear();
-            for(int i =  0; i < mChildren.Count; i++)
+            for (int i = 0; i < mChildren.Count; i++)
             {
                 mPanel.Children.Add(mChildren[i]);
             }
         }
+
+        public void AddDataGrid(string label, Dictionary<String, Object> data, int indexPos = -1)
+        {
+            AddUIElement(label, new DictionaryGridView(data), indexPos);
+        }
+
+        public void AddPreviewPanel(string label, BitmapSource bitmap, int indexPos = -1)
+        {
+            Image bitmapImage = new Image()
+            {
+                Source = bitmap,
+                Width = bitmap.Width,
+                Height = bitmap.Height,
+            };
+
+            DrawingBrush BackgroundPNG = new DrawingBrush()
+            {
+                TileMode = TileMode.Tile,
+                Viewport = new Rect(0, 0, 32, 32),
+                ViewportUnits = BrushMappingMode.Absolute,
+                Drawing = new GeometryDrawing()
+                {
+                    Geometry = Geometry.Parse("M0,0 H1 V1 H2 V2 H1 V1 H0Z"),
+                    Brush = new SolidColorBrush(Colors.LightGray),
+                }
+            };
+
+            Grid grid = new Grid()
+            {
+                Background = BackgroundPNG,
+                Children = { bitmapImage },
+                Height = bitmap.Height + 20,
+                Width = bitmap.Width + 20
+            };
+
+            AddUIElement(label, grid, indexPos);
+        }       
     }
 }
